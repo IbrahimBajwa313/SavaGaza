7c790067d7600c5f567b5f07cb15ed150d5953c0
saadasif04 <asifnawazmalik75@gmail.com>
2024-10-04 19:34:39 +0500

    changes

--- pages/ImageSec.jsx
+++ pages/ImageSec.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function ImageSec() {
   return (
-    <div className='min-h-screen'>
-      <div className="relative h-screen">
-        <div className="absolute inset-0 bg-[url('/ImageSec/bg.jpg')] bg-cover bg-center brightness"></div>
+    <div className=''>
+      <div className="relative">
+        <div className="absolute inset-0 bg-[url('/ImageSec/bg.jpg')] bg-cover bg-center filter brightness"></div>
 
         <div className="images w-full h-full flex justify-center items-center relative">
-          <img src="/ImageSec/pic.png" className='w-full px-2' alt="Image" />
+          <img src="/ImageSec/pic.png" className='w-full px-5 py-2' alt="Image" />
         </div>
       </div>
     </div>
